e7d91bee58d57ee65284b8a88a19cb760155e59e
Keith Kirtfield <keithryankirtfield@gmail.com>
2019-07-09 13:06:43 -0400

    woring on newsletter routes

--- Routes/Newsletter.js
+++ Routes/Newsletter.js
@@ -20,10 +20,10 @@ module.exports = app => {
       return res.status(404).json({ err: err.code });
     }
   });
-  app.post("/newsletter", async (req, res) => {
+  app.post("/newsletter", auth, async (req, res) => {
     try {
       const { email } = req.body;
-      if (email === "") {
+      if (email.trim() === "") {
         return res.status(400).json({ msg: "email cannot be empty" });
       }
       const newEmail = {
@@ -32,15 +32,38 @@ module.exports = app => {
       };
       const repeatEmail = await database
         .collection("Newsletter")
-        .where("email", "==", email)
+        .where("email", "==", newEmail.email)
         .get();
-      if (repeatEmail)
-        return res.status(400).json({ msg: "Email already in newsletter" });
+      repeatEmail.forEach(doc => {
+        if (doc.data().email == newEmail.email)
+          return res.status(400).json({ msg: "email already used" });
+      });
+
       await database.collection("Newsletter").add(newEmail);
       return res.json({ msg: `${email} added!` });
     } catch (err) {
       console.log(err);
       return res.status(502).json({ err });
+    }
+  });
+  app.delete("/newsletter/:email", async (req, res) => {
+    try {
+      const email = await database
+        .collection("Newsletter")
+        .where("email", "==", req.params.email)
+        .get();
+      // if (!email.empty)
+      //   return res.status(404).json({ msg: "Email not in database" });
+      email.forEach(async doc => {
+        await database
+          .doc(`Newsletter/${doc.id}`)
+          // .get()
+          .delete();
+          console.log(doc.id);
+      });
+      return res.status(200).json({ msg: `${req.params.email} deleted` });
+    } catch (err) {
+      return res.status(404).json({ err });
     }
   });
 };
