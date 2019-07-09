bd7763e0e27e564f821acd0d2f3eb4454840b76b
Keith Kirtfield <keithryankirtfield@gmail.com>
2019-07-09 17:52:05 -0400

    FInished outsource link

--- Routes/Outsource.js
+++ Routes/Outsource.js
@@ -1,3 +1,59 @@
-module.exports = app =>{
-    
-}
+const { database } = require("../Config/admin");
+const auth = require("../Helper/auth");
+const isEmpty = require("../Helper/isEmpty");
+module.exports = app => {
+  // recieving Links
+  app.get("/outsourcelinks", auth, async (req, res) => {
+    try {
+      let links = [];
+      const favBlogs = await database.collection("OutSource").get();
+      favBlogs.forEach(doc => {
+        const data = {
+          values: doc.data(),
+          id: doc.id
+        };
+        links.push(data);
+      });
+      return res.send(links);
+    } catch (err) {
+      console.log(err);
+      return res.status(404).json({ err });
+    }
+  });
+
+  //   Adding Link
+  app.post("/outsourcelinks", auth, async (req, res) => {
+    try {
+      let error = [];
+      const { link, title, desc } = req.body;
+      if (isEmpty(link)) error.push("Must have a link");
+      if (isEmpty(desc)) error.push("Must have a desc");
+      if (isEmpty(title)) error.push("Must have a title");
+      if (error.length > 0) return res.status(405).json(error);
+      const newPost = {
+        link,
+        title,
+        desc,
+        data: new Date().toISOString()
+      };
+      await database.collection("OutSource").add(newPost);
+      return res.status(200).json(newPost);
+    } catch (err) {
+      console.log(err);
+      return res.status(404).json({ err: err.code });
+    }
+  });
+  //   Deleting a Link
+  app.delete("/outsourcelinks/:id", async (req, res) => {
+    try {
+      const link = await database.doc(`OutSource/${req.params.id}`).get();
+      if (link == "undefined")
+        return res.status(404).json({ msg: "Link not found" });
+      await database.doc(`OutSource/${req.params.id}`).delete();
+      return res.status(200).json({ msg: `${req.params.id} deleted` });
+    } catch (err) {
+      console.log(err);
+      return res.status(404).json({ err: err.code });
+    }
+  });
+};
